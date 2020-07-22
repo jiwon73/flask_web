0463893646006246a4eb44bab0ae8a6ead5a71f1
Your Name <you@example.com>
2020-07-22 17:43:51 +0900

    register step3 and login step1

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
 from flask import Flask ,render_template , flash , redirect , url_for, session, request, logging
-#from flask_mysqldb import MySQL
-import pymysql
-from data import Articles
-from passlib.hash import pbkdf2_sha256
 
+import pymysql
+from passlib.hash import pbkdf2_sha256
+from data import Articles
 
 app = Flask(__name__)
 app.debug=True
 
-#config MySQL
-# app.config['MYSQL_HOST']='localhost'
-# app.config['MYSQL_USER']='root'
-# app.config['MYSQL_PASSWORD']='1234'
-# app.config['MYSQL_DB']='myflaskapp'
-# app.config['MYSQL_CURSORCLASS']='DictCursor'
 
 db = pymysql.connect(host='localhost', 
                         port=3306, 
                         user='root', 
                         passwd='1234', 
                         db='myflaskapp')
-                        
+
+
 #init mysql 
 # mysql = MySQL(app)
 # cur  = mysql.connection.cursor()
@@ -36,50 +30,72 @@ def index():
     # return "TEST"
     return render_template('home.html',hello="GaryKim")
 
-@app.route('/register',methods=['GET','POST'])
-def register():
-    if request.method == 'POST':
-
-        # data=request.body.get('author')
-        name=request.form.get('name')
-        email=request.form.get('email')
-        password=pbkdf2_sha256.hash(request.form.get('password'))
-        re_password=request.form.get('re_password')
-        username=request.form.get('username')
-        # name=form.name.data
-        
-        #if(password==re_password)
-        if (pbkdf2_sha256.verify(re_password,password)):
-            print(pbkdf2_sha256.verify(re_password,password))
-            #print([name, email, password, re_password, username])
-            
-            cursor=db.cursor()
-
-            sql=  '''
-                    INSERT INTO users(name, email , username, password) 
-                    VALUES (%s ,%s, %s,%s);
-                        '''
-            cursor.execute(sql,(name,email,username,password))
-            db.commit()
-            
-
-            # cursor.execute('SELECT* FROM users;')
-            # users=cursor.fetchall()
-            # return users 
-            # db.close()
-
-            return "Register Success"
-        else: 
-            return "Invalid Password"
-        db.close()   
-    else:
-        return render_template('register.html')
-
 @app.route('/about')
 def about():
     print("Success")
     # return "TEST"
     return render_template('about.html',hello="GaryKim")
+
+@app.route('/register',methods=['GET' ,'POST'])
+def register():
+    if request.method == 'POST':
+
+        # data = request.body.get('author')
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = pbkdf2_sha256.hash(request.form.get('password'))
+        re_password = request.form.get('re_password')
+        username = request.form.get('username')
+        # name = form.name.data
+        if(pbkdf2_sha256.verify(re_password,password )):
+            print(pbkdf2_sha256.verify(re_password,password ))
+            cursor = db.cursor()
+            sql = '''
+                INSERT INTO users (name , email , username , password) 
+                VALUES (%s ,%s, %s, %s )
+             '''
+            cursor.execute(sql , (name,email,username,password ))
+            db.commit()
+            
+
+            # cursor = db.cursor()
+            # cursor.execute('SELECT * FROM users;')
+            # users = cursor.fetchall()
+            
+            return "register Success"
+
+        else:
+            return "Invalid Password"
+
+        db.close()
+    else:
+        return render_template('register.html')
+
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        id = request.form.get('email')
+        pw = request.form.get('password')
+        # print([id , pw])
+
+        sql='SELECT * FROM users WHERE email = %s'
+        cursor  = db.cursor()
+        cursor.execute(sql, [id])
+        users = cursor.fetchone()
+        print(users)
+
+        if users ==None:
+            return redirect(url_for('login'))
+        else:
+            if pbkdf2_sha256.verify(pw,users[4] ):
+                return redirect(url_for('articles'))
+            else:
+                return redirect(url_for('login'))
+        
+        # return "Success"
+    else:
+        return render_template('login.html')
 
 @app.route('/articles', methods=['GET','POST'])
 def articles():
@@ -104,4 +120,3 @@ def article(id):
 if __name__ =='__main__':
     # app.run(host='0.0.0.0', port='8080')
     app.run()
-    
